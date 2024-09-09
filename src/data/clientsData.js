7e5e26a5c015484919a8527ca076fd17afc2f68a
GodLikeGill <jatin.singhgill37@gmail.com>
2024-09-09 03:42:27 -0400

    Added few more technologies

--- src/data/clientsData.js
+++ src/data/clientsData.js
@@ -1,10 +1,18 @@
 // Import images
-import java from '../images/technologies/java-logo.png';
-import swift from '../images/technologies/swift-logo.png';
-import javascript from '../images/technologies/javascript-logo.png';
-import androidStudio from '../images/technologies/android-studio-logo.png';
+import cpp from '../images/technologies/cpp.png';
+import css from '../images/technologies/css.png';
+import java from '../images/technologies/java.png';
+import swift from '../images/technologies/swift.png';
+import html5 from '../images/technologies/html5.png';
+import xcode from '../images/technologies/xcode.png';
+import unity from '../images/technologies/unity.png';
+import csharp from '../images/technologies/csharp.png';
+import kotlin from '../images/technologies/kotlin.png';
+import intellij from '../images/technologies/intellij.png';
+import javascript from '../images/technologies/javascript.png';
+import androidStudio from '../images/technologies/android-studio.png';
 
-export const clientsHeading = 'Some of the technoligies I have worked on';
+export const clientsHeading = 'Some of the technologies I have worked on';
 
 export const clientsData = [
 	{
@@ -26,5 +34,45 @@ export const clientsData = [
 		id: 4,
 		title: 'Swift',
 		img: swift,
+	},
+	{
+		id: 5,
+		title: 'Kotlin',
+		img: kotlin,
+	},
+	{
+		id: 6,
+		title: 'C#',
+		img: csharp,
+	},
+	{
+		id: 7,
+		title: 'HTML5',
+		img: html5,
+	},
+	{
+		id: 8,
+		title: 'Intellij',
+		img: intellij,
+	},
+	{
+		id: 9,
+		title: 'Xcode',
+		img: xcode,
+	},
+	{
+		id: 10,
+		title: 'Unity',
+		img: unity,
+	},
+	{
+		id: 6,
+		title: 'C++',
+		img: cpp,
+	},
+	{
+		id: 6,
+		title: 'CSS',
+		img: css,
 	},
 ];
